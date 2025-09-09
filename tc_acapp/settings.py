617929f1c11f934b262d1e26cf4d673a180ae4fd
ppshuX <2064747320@qq.com>
2025-09-09 20:39:18 +0800

    create project structure

--- tc_acapp/settings.py
+++ tc_acapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['139.129.36.243']
 # Application definition
 
 INSTALLED_APPS = [
+    'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
