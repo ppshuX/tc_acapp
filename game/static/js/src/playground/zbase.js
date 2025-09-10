8e1a273b2191ee9c6be8a92a1c18fa1f93fbf0b7
ppshuX <2064747320@qq.com>
2025-09-10 20:44:01 +0800

    lesson 4 create the game

--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -1,19 +1,29 @@
 class AcGamePlayground{
     constructor(root){
         this.root = root;
-        this.$playground = $(`<div>Playground</div>`);
+        this.$playground = $(`<div class="ac-game-playground"></div>`);
 
-        this.hide();
+        //this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.width = this.$playground.width();
+        this.height = this.$playground.height();
+        this.game_map = new GameMap(this);
+        this.players = [];
+        this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, "white", this.height * 0.15, true));
+
+        for (let i = 0 ; i < 6 ; i ++) {
+            this.players.push(new Player(this, this.width / 2, this.height / 2, this.height * 0.05, this.get_random_color(), this.height * 0.15, false));
+        }
 
         this.start();
     }
 
-    start(){
-
+    get_random_color() {
+        let colors = ["blue", "red", "pink", "grey", "green", "yellow"];
+        return colors[Math.floor(Math.random() * 6)];
     }
 
-    update(){
+    start(){
 
     }
 
