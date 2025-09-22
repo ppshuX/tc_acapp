884cdbf3c73db27985086a6e50f5b36b5adac104
wenxiao Lv <2064747320@qq.com>
2025-09-22 11:11:09 +0800

    corrections qq_login -> jwt

--- game/views/settings/qq/receive_code.py
+++ game/views/settings/qq/receive_code.py
@@ -1,10 +1,10 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, reverse
 from django.core.cache import cache
 import requests
 from django.contrib.auth.models import User
-from django.contrib.auth import login
 from game.models.player.player import Player
 from random import randint
+from rest_framework_simplejwt.tokens import RefreshToken
 
 def receive_code(request):
     data = request.GET
@@ -15,47 +15,66 @@ def receive_code(request):
     redirect_uri = "https://app7581.acapp.acwing.com.cn/settings/qq/receive_code"  # 此处不用处理网址中的特殊字符
 
     if not cache.has_key(state):  # 若无此state返回主页面
-        return redirect("index")
+        return redirect(reverse("index"))
 
     cache.delete(state)
 
 
-    apply_access_token_url = "https://graph.qq.com/oauth2.0/token"
-    params = {
-        'grant_type': 'authorization_code',
-        'client_id': "102808975",                            # 填入APP ID
-        'client_secret': "PgiU0OYXIn49za1E",                 # 填入APP Key
-        'code': code,
-        'redirect_uri': redirect_uri,
-        'fmt': "json"
-    }
-    access_token_res = requests.get(apply_access_token_url, params=params).json()
-    access_token = access_token_res['access_token']
+    try:
+        apply_access_token_url = "https://graph.qq.com/oauth2.0/token"
+        params = {
+            'grant_type': 'authorization_code',
+            'client_id': "102812476",                            # 填入APP ID
+            'client_secret': "10nLehS1MRN7FAut",                 # 填入APP Key
+            'code': code,
+            'redirect_uri': redirect_uri,
+            'fmt': "json"
+        }
+        access_token_res = requests.get(apply_access_token_url, params=params).json()
+        
+        if 'access_token' not in access_token_res:
+            return redirect(reverse("index"))
+        
+        access_token = access_token_res['access_token']
 
-    apply_openid_url = "https://graph.qq.com/oauth2.0/me"
-    params = {
-        'access_token': access_token,
-        'fmt': "json"
-    }
-    openid_res = requests.get(apply_openid_url, params=params).json()
-    openid = openid_res['openid']
+        apply_openid_url = "https://graph.qq.com/oauth2.0/me"
+        params = {
+            'access_token': access_token,
+            'fmt': "json"
+        }
+        openid_res = requests.get(apply_openid_url, params=params).json()
+        
+        if 'openid' not in openid_res:
+            return redirect(reverse("index"))
+            
+        openid = openid_res['openid']
+    except Exception as e:
+        return redirect(reverse("index"))
 
 
     players = Player.objects.filter(openid=openid)
     if players.exists():  # 如果该用户已存在，则无需获取信息，直接登录
-        login(request, players[0].user)
-        return redirect("index")
+        player = players[0]
+        refresh = RefreshToken.for_user(player.user)
+        return redirect(reverse("index") + "?access=%s&refresh=%s" % (str(refresh.access_token), str(refresh)))
 
 
-    get_user_info_url = "https://graph.qq.com/user/get_user_info"
-    params = {
-        'access_token': access_token,
-        'oauth_consumer_key': "102808975",      # 填入APP ID
-        'openid': openid
-    }
-    userinfo_res = requests.get(get_user_info_url, params=params).json()
-    username = userinfo_res['nickname']
-    photo = userinfo_res['figureurl_qq_1']
+    try:
+        get_user_info_url = "https://graph.qq.com/user/get_user_info"
+        params = {
+            'access_token': access_token,
+            'oauth_consumer_key': "102812476",      # 填入APP ID
+            'openid': openid
+        }
+        userinfo_res = requests.get(get_user_info_url, params=params).json()
+        
+        if 'nickname' not in userinfo_res:
+            return redirect(reverse("index"))
+            
+        username = userinfo_res['nickname']
+        photo = userinfo_res['figureurl_qq_1']
+    except Exception as e:
+        return redirect(reverse("index"))
 
 
     while User.objects.filter(username=username).exists():  # 给重名的用户找一个不重名的名字
@@ -64,6 +83,6 @@ def receive_code(request):
     user = User.objects.create(username=username)
     player = Player.objects.create(user=user, photo=photo, openid=openid)
 
-    login(request, user)
+    refresh = RefreshToken.for_user(user)
 
-    return redirect("index")
+    return redirect(reverse("index") + "?access=%s&refresh=%s" % (str(refresh.access_token), str(refresh)))
